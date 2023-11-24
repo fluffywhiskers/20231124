743227dfe975ab03ddde5e608259bfde90b591ae
fluffywhiskers <143849718+fluffywhiskers@users.noreply.github.com>
2023-11-24 12:18:16 -0300

    Add files via upload

---
+++ atividade24.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class atividade24
+{   // IF ELSE
+	/*public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Informe seu login: ");
+        String loginUser = sc.nextLine();
+        System.out.println("Informe sua senha: ");
+        String passUser = sc.nextLine();
+
+        String stanLogin = "usuario3002@gmail.com";
+        String stanPass = "senha123";
+
+        if (loginUser.equals(stanLogin) && passUser.equals(stanPass)) {
+            System.out.println("Login bem sucedido. Bem-vindo!");
+        } else {
+            System.out.println("Login ou senha incorretos.");
+        }
+
+	}*/
+	/*public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Informe o primeiro valor: ");
+	    double n1 = sc.nextDouble();
+	    System.out.println("Informe o segundo valor: ");
+	    double n2 = sc.nextDouble();
+	    
+	    double soma = n1 + n2;
+	    
+	    System.out.println("A soma dos números: " + n1 + " + " + n2 + " é: " + soma);
+
+        sc.close();
+        
+	}*/
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite algo: ");
+        String a = sc.nextLine();
+
+        sc.close();
+
+        for (char caracter : a.toCharArray()) {
+            if (Character.isDigit(caracter)) {
+                System.out.println("Existe um caracter numérico");
+            }
+        }
+    }*/
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite sua idade: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        if (n >= 18) {
+            System.out.println("Você é maior de idade");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }
+    */
+    /*public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe a altura da caixa: ");
+        double a = sc.nextDouble();
+
+        double aMin = 100.0;
+        double aMax = 1000.0;
+
+        sc.close();
+
+        if (a >= aMin && a <= aMax) {
+            System.out.println("Você está na altura permitida");
+        } else {
+            System.out.println("Altura inválida");
+        }
+
+        System.out.println("Altura informada: " + a);
+    }
+    */
+
+}
